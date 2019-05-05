400709fa28ec26d5583874c156ed2c59937df7ec
ibtisam midlet <storebitid@gmail.com>
2019-05-05 19:33:54 +0200

    Create youtube-watched-videos-hider.meta.js

---
+++ youtube-watched-videos-hider.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        youtube watched videos hider
+// @version     1.0
+// @updateURL   https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.meta.js
+// @downloadURL https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.user.js
+// ==/UserScript==
