6f68188182027308df3fdf7cc965a4e3f0bce2ab
ibtisam midlet <storebitid@gmail.com>
2020-03-18 06:05:13 +0100

    Update youtube-watched-videos-hider.user.js

--- youtube-watched-videos-hider.user.js
+++ youtube-watched-videos-hider.user.js
@@ -5,7 +5,7 @@
 // @icon        https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/screenshot.png
 // @include     *.youtube.com/*
 // @description like the name
-// @version     1.0
+// @version     1.7
 // @updateURL   https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.meta.js
 // @downloadURL https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.user.js
 // @supportURL  https://rebrand.ly/wanna-me
