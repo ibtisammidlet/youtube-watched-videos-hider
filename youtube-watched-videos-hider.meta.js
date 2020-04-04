66438e06e90ae6cb1db4da29b405ca80353ecef7
ibtisam midlet <storebitid@gmail.com>
2020-04-04 21:21:05 +0200

    Update youtube-watched-videos-hider.meta.js

--- youtube-watched-videos-hider.meta.js
+++ youtube-watched-videos-hider.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        youtube watched videos hider
-// @version     2.0
+// @version     2.2
 // @updateURL   https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.meta.js
 // @downloadURL https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.user.js
 // ==/UserScript==
