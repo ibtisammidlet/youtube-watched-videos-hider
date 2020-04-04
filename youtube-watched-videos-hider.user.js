835845973c0318376dd6080882bc90f515d0b9a8
ibtisam midlet <storebitid@gmail.com>
2020-04-04 21:22:28 +0200

    Update youtube-watched-videos-hider.user.js

--- youtube-watched-videos-hider.user.js
+++ youtube-watched-videos-hider.user.js
@@ -4,8 +4,8 @@
 // @namespace   youtube-watched-videos-hider-userscript-bystorebitid@gmail.com
 // @icon        https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/screenshot.png
 // @include     *.youtube.com/*
-// @description like the name and it's simple and effective
-// @version     2.0
+// @description like the name and it's simple stable and effective
+// @version     2.2
 // @updateURL   https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.meta.js
 // @downloadURL https://github.com/ibtisammidlet/youtube-watched-videos-hider/raw/master/youtube-watched-videos-hider.user.js
 // @supportURL  https://justinmidlet.blogspot.com/2019/05/youtube-watched-videos-hider.html
@@ -47,8 +47,18 @@ $('#overlays.ytd-thumbnail #progress').parent().parent().parent().parent().paren
 
 function watch(){
 if (window.location.href.indexOf('www.youtube.com/watch?v=') != -1) {
+
+var playlisttxtcntnt = $("#playlist").text();
+//* checking if the side playlist length is less then 11 otherwise the same user content will be checked
+if (playlisttxtcntnt.length < "15") {
+$('#overlays.ytd-thumbnail #progress').parent().parent().parent().parent().parent().parent().hide();
+} else {
+//* hiding the related + playlist
 $('#overlays.ytd-thumbnail #progress').parent().parent().parent().parent().parent().parent().hide();
 $("#playlist #overlays.ytd-thumbnail #progress").parent().parent().parent().parent().parent().parent().parent().parent().hide();
+}
+
+
 }
 };
 
